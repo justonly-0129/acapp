a2a9716085451f745c8bca6fc0ec26c77685c78b
justonly-0129 <57661623+justonly-0129@users.noreply.github.com>
2022-01-18 11:57:35 +0800

    Update game.js

--- game/static/js/dist/game.js
+++ game/static/js/dist/game.js
@@ -1,87 +1,89 @@
 class AcGameMenu {
-	constructor(root) {
-		this.root = root;
-		this.$menu = $(`
-<div class= "ac-game-menu">
-	<div class="ac-game-menu-field">
-		<div class="ac-game-menu-field-item ac-game-menu-field-item-single-mode">
-			å•äººæ¨¡å¼
-		</div>
-		<br>
-		<div class="ac-game-menu-field-item ac-game-menu-field-item-multi-mode">
-			å¤šäººæ¨¡å¼
-		</div>
-		<br>
-		<div class="ac-game-menu-field-item ac-game-menu-field-item-settings">
-			è®¾ç½®
-		</div>
-	</div>
+    constructor(root) {
+        this.root = root;
+        this.$menu = $(`
+<div class="ac-game-menu">
+    <div class="ac-game-menu-field">
+        <div class="ac-game-menu-field-item ac-game-menu-field-item-single-mode">
+            单人模式
+        </div>
+        <br>
+        <div class="ac-game-menu-field-item ac-game-menu-field-item-multi-mode">
+            多人模式
+        </div>
+        <br>
+        <div class="ac-game-menu-field-item ac-game-menu-field-item-settings">
+            设置
+        </div>
+    </div>
 </div>
+`);
+        this.root.$ac_game.append(this.$menu);
+        this.$single_mode = this.$menu.find('.ac-game-menu-field-item-single-mode');
+        this.$multi_mode = this.$menu.find('.ac-game-menu-field-item-multi-mode');
+        this.$settings = this.$menu.find('.ac-game-menu-field-item-settings');
 
-`);      //htmå¯¹è±¡åŠ Â¥æ™®é€šå¯¹è±¡ä¸åŠ 
-		this.root.$ac_game.append(this.$menu);	
-		this.$single_mode = this.$menu.find('.ac-game-menu-field-item-single-modeâ');
-		this.$multi_mode = this.$menu.find('.ac-game-menu-field-item-multi-mode');
-		this.$settings = this.$menu.find('.ac-game-menu-field-item-settings');
-	
-		this.start();
-	}
-		start() {
-			this.add_listening_events();
-		}
-		add_listening_events() {
-			let outer = this;
-			this.$single_mode.click(function() {
-				outer.hide();
-				outer.root.playground.show();
-			});
-			this.$multi_mode.click(function(){
-				console.log("click multi mode");
-			});
-			this.$settings.click(function(){
-				console.log("click settings");
-			});
-	}
-		show() { //æ˜¾ç¤ºmenuç•Œé¢
-			this.$menu.show();
-		}
-		hide() { //å…³é—­menuç•Œé¢
-			this.$menu.hide();
-		}
+        this.start();
+    }
 
+    start() {
+        this.add_listening_events();
+    }
 
+    add_listening_events() {
+        let outer = this;
+        this.$single_mode.click(function(){
+            outer.hide();
+            outer.root.playground.show();
+        });
+        this.$multi_mode.click(function(){
+            console.log("click multi mode");
+        });
+        this.$settings.click(function(){
+            console.log("click settings");
+        });
+    }
+
+    show() {  // 显示menu界面
+        this.$menu.show();
+    }
+
+    hide() {  // 关闭menu界面
+        this.$menu.hide();
+    }
 }
 class AcGamePlayground {
-	constructor(root) {
-		this.root = root;
-		this.$playground = $(<div>æ¸¸æˆç•Œé¢</div>);
-		
-		this.hide();
-		this.root.$ac_game.append(this.$playground);
+    constructor(root) {
+        this.root = root;
+        this.$playground = $(`<div>游戏界面</div>`);
 
-		this.start();
-	}
+        this.hide();
+        this.root.$ac_game.append(this.$playground);
 
-	start() {
-	}
+        this.start();
+    }
 
-	show() { //æ‰“å¼€playgroundç•Œé¢
-		this.$playground.show();
-	}
-	hide() { //å…³é—­playgroundç•Œé¢
-		this.$playground.hide();
-	}
+    start() {
+    }
+
+    show() {  // 打开playground界面
+        this.$playground.show();
+    }
+
+    hide() {  // 关闭playground界面
+        this.$playground.hide();
+    }
 }
 class AcGame {
-	constructor(id) {
-		this.id = id;
-		this.$ac_game = $('#' + id);
-		this.menu = new AcGameMenu(this);
-		this.playground = new AcGamePlayground(this);
-		
-		this.start();
-	}
+    constructor(id) {
+        this.id = id;
+        this.$ac_game = $('#' + id);
+        this.menu = new AcGameMenu(this);
+        this.playground = new AcGamePlayground(this);
 
-	start() {
-	}
+        this.start();
+    }
+
+    start() {
+    }
 }
