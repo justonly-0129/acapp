15d6f4ba50d26541ca9ad928e6feb13a6c0e5e89
justonly-0129 <57661623+justonly-0129@users.noreply.github.com>
2022-01-19 19:51:22 +0800

    Update game.js

--- game/static/js/dist/game.js
+++ game/static/js/dist/game.js
@@ -425,15 +425,16 @@ class AcGamePlayground {
 	}
 }
 export class AcGame {
-	constructor(id) {
-		this.id = id;
-		this.$ac_game = $('#' + id);
-		//this.menu = new AcGameMenu(this);
-		this.playground = new AcGamePlayground(this);
-		
-		this.start();
-	}
+    constructor(id) {
+        this.id = id;
+        this.$ac_game = $('#' + id);
+        this.menu = new AcGameMenu(this);
+        // this.menu = new AcGameMenu(this);
+        this.playground = new AcGamePlayground(this);
 
-	start() {
-	}
+        this.start();
+    }
+
+    start() {
+    }
 }
