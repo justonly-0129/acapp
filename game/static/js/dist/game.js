2306289d2f2d85065e7b6d8375432d4c7bf03fee
justonly-0129 <57661623+justonly-0129@users.noreply.github.com>
2022-01-21 11:17:12 +0800

    Update game.js

--- game/static/js/dist/game.js
+++ game/static/js/dist/game.js
@@ -433,7 +433,6 @@ export class AcGame {
         this.id = id;
         this.$ac_game = $('#' + id);
         this.menu = new AcGameMenu(this);
-        this.menu = new AcGameMenu(this);
         this.playground = new AcGamePlayground(this);
 
         this.start();
