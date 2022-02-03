acf5d2cfc0a150df1e18740b2bbdd26ff8feb889
justonly-0129 <a15656153915@163.com>
2022-02-03 15:50:53 +0800

    implement multi mode and blink and coldtime

--- game/static/js/src/playground/skill/fireball/zbase.js
+++ game/static/js/src/playground/skill/fireball/zbase.js
@@ -24,21 +24,30 @@ class FireBall extends AcGameObject {
             this.destroy();
             return false;
         }
-
-        let moved = Math.min(this.move_length, this.speed * this.timedelta / 1000);
-        this.x += this.vx * moved;
-        this.y += this.vy * moved;
-        this.move_length -= moved;
-
-        for (let i = 0; i < this.playground.players.length; i ++ ) {
-            let player = this.playground.players[i];
-            if (this.player !== player && this.is_collision(player)) {
-                this.attack(player);
-            }
-        }
+		
+		this.update_move();
+		this.update_attack();
 
         this.render();
     }
+
+
+	update_move() {
+		let moved = Math.min(this.move_length, this.speed * this.timedelta / 1000);
+		this.x += this.vx * moved;
+		this.y += this.vy * moved;
+		this.moved_length -= moved;
+	}
+
+	update_attack() {
+		for (let i = 0; i < this.playground.players.length; i++) {
+			let player = this.playground.players[i];
+			if (this.player !== player && this.is_collision(player)) {
+				this.attack(player);
+				break;
+			}
+		}
+	}
 
     get_dist(x1, y1, x2, y2) {
         let dx = x1 - x2;
@@ -56,6 +65,11 @@ class FireBall extends AcGameObject {
     attack(player) {
         let angle = Math.atan2(player.y - this.y, player.x - this.x);
         player.is_attacked(angle, this.damage);
+
+		if (this.playground.mode === "multi mode") {
+			this.playground.mps.send_attack(player.uuid, player.x, player.y, angle, this.damage, this.uuid);
+		}
+
         this.destroy();
     }
 
@@ -68,4 +82,14 @@ class FireBall extends AcGameObject {
         this.ctx.fillStyle = this.color;
         this.ctx.fill();
     }
+
+	on_destroy() {
+		let fireballs = this.player.fireballs;
+		for (let i = 0; i < fireballs.length; i++) {
+			if (fireballs[i] === this) {
+				fireballs.splice(i, 1);
+				break;
+			}
+		}
+	}
 }
